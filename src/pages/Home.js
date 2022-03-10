fd68a22bc00c134742865c2e3b2dc45338d05b6b
Emerson <emerapmelo@gmail.com>
2022-03-10 18:28:44 -0300

    [Grupo 11] Requisito 4

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,50 @@
 import React from 'react';
+import { getCategories } from '../services/api';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+
+    this.state = ({
+      categories: [],
+    });
+
+    this.categories = async () => {
+      const categoriesList = await getCategories();
+      this.setState({
+        categories: categoriesList,
+      });
+    };
+  }
+
+  componentDidMount() {
+    this.categories();
+  }
+
   render() {
+    const { categories } = this.state;
     return (
-      <div>
-        <h1 data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </h1>
-      </div>
+      <>
+        <div>
+          { categories.map((categorie) => (
+            <div key={ categorie.name }>
+              <input
+                data-testid="category"
+                type="radio"
+                id="categorie"
+              />
+              <label htmlFor="categorie">{categorie.name}</label>
+            </div>
+          ))}
+        </div>
+
+        <div>
+          <h1 data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </h1>
+        </div>
+      </>
+
     );
   }
 }
